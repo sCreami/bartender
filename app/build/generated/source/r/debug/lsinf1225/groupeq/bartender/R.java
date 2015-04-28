0125a576ab3fccb900716ffbe5f4c626970cb1d0
Oli <olivier.martin@student.uclouvain.be>
2015-04-28 17:50:04 +0200

    Accueil XML

--- app/build/generated/source/r/debug/lsinf1225/groupeq/bartender/R.java
+++ app/build/generated/source/r/debug/lsinf1225/groupeq/bartender/R.java
@@ -11,29 +11,38 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
+    }
+    public static final class drawable {
+        public static final int custom_btn_opal=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090004;
+        public static final int buttonCarte=0x7f090000;
+        public static final int buttonFacture=0x7f090001;
+        public static final int buttonMusique=0x7f090002;
+        public static final int buttonOption=0x7f090003;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
+        public static final int accueil=0x7f040000;
+        public static final int activity_main=0x7f040001;
     }
     public static final class menu {
-        public static final int menu_main=0x7f070000;
+        public static final int menu_main=0x7f080000;
     }
     public static final class mipmap {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_launcher=0x7f030000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050000;
-        public static final int app_name=0x7f050001;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060000;
+        public static final int app_name=0x7f060001;
+        public static final int hello_world=0x7f060002;
     }
     public static final class style {
         /**  Customize your theme here. 
          */
-        public static final int AppTheme=0x7f060000;
+        public static final int AppTheme=0x7f070000;
+        public static final int btnStyleopal=0x7f070001;
     }
 }
