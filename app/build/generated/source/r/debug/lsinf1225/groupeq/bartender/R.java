92756792d4cce0f9d75eb35e9394cb07a7ca2310
Oli <olivier.martin@student.uclouvain.be>
2015-04-29 16:37:00 +0200

    Connexion XML

--- app/build/generated/source/r/debug/lsinf1225/groupeq/bartender/R.java
+++ app/build/generated/source/r/debug/lsinf1225/groupeq/bartender/R.java
@@ -18,7 +18,7 @@ public final class R {
         public static final int custom_btn_opal=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090009;
+        public static final int action_settings=0x7f09000b;
         public static final int buttonAjouterProduit=0x7f090007;
         public static final int buttonCarte=0x7f090000;
         public static final int buttonFacture=0x7f090001;
@@ -26,13 +26,16 @@ public final class R {
         public static final int buttonMusique=0x7f090002;
         public static final int buttonOption=0x7f090004;
         public static final int buttonRetourProduit=0x7f090008;
-        public static final int calcul=0x7f090005;
-        public static final int raz=0x7f090006;
+        public static final int calcul=0x7f090009;
+        public static final int connexionLogin=0x7f090005;
+        public static final int connextionMDP=0x7f090006;
+        public static final int raz=0x7f09000a;
     }
     public static final class layout {
         public static final int accueil=0x7f040000;
         public static final int activity_main=0x7f040001;
-        public static final int produit=0x7f040002;
+        public static final int connexion=0x7f040002;
+        public static final int produit=0x7f040003;
     }
     public static final class menu {
         public static final int menu_main=0x7f080000;
